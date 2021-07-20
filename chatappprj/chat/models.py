05252665699f5d2282d6ad32a2b0ee86186d3e34
Destiny Franks <desphixs@gmail.com>
2021-07-19 21:23:27 -1100

    Django Chat App

---
+++ chatappprj/chat/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+
+class Message(models.Model):
+    value = models.CharField(max_length=10000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    room = models.CharField(max_length=1000000)
+    user = models.CharField(max_length=1000000)
---
+++ chatappprj/chat/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .models import Room, Message
+from django.http import HttpResponse, JsonResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def room(request, room):
+    username = request.GET.get('username') # henry
+    room_details = Room.objects.get(name=room)
+    return render(request, 'room.html', {
+
+        'username': username,
+        'room': room,
+        'room_details': room_details,
+    })
+
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    # return HttpResponse("Hi, Message Sent Successfully!!")
+
+def getMessages(request,  room):
+    room_details = Room.objects.get(name=room)
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages": list(messages.values())})
